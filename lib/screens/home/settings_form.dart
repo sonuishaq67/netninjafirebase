6295f375f75fe636929c238c908432b994087c70
sonuishaq67 <sonuishaq67@gmail.com>
2020-08-25 18:34:16 +0530

    Tuesday 25 August 2020 06:34:16 PM IST

--- lib/screens/home/settings_form.dart
+++ lib/screens/home/settings_form.dart
@@ -23,6 +23,62 @@ class _SettingsFormState extends State<SettingsForm> {
           ),
           SizedBox(height: 20),
           TextFormField(
+            decoration: InputDecoration(
+              hintText: 'email',
+              fillColor: Colors.white,
+              filled: true,
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white, width: 2.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.pink, width: 2.0)),
+            ),
+            validator: (val) => val.isEmpty ? 'Please enter a name' : null,
+            onChanged: (val) => setState(() => _currName = val),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          //dropdown
+          DropdownButtonFormField(
+            decoration: InputDecoration(
+              hintText: 'email',
+              fillColor: Colors.white,
+              filled: true,
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white, width: 2.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.pink, width: 2.0)),
+            ),
+            value: _currSugars ?? '0',
+            items: sugars.map((sugar) {
+              return DropdownMenuItem(
+                  value: sugar, child: Text('sugar $sugar'));
+            }).toList(),
+            onChanged: (val) => setState(() => _currSugars = val),
+          ),
+          Slider(
+              min: 100,
+              max: 900,
+              activeColor: Colors.brown[_currStren ?? 100],
+              inactiveColor: Colors.brown[_currStren ?? 100],
+              divisions: 8,
+              value: (_currStren ?? 100).toDouble(),
+              onChanged: (val) => setState(() {
+                    _currStren = val.round();
+                  })),
+          RaisedButton(
+            color: Colors.pink,
+            child: Text(
+              'update',
+              style: TextStyle(color: Colors.white),
+            ),
+            onPressed: () async {
+              print(_currName);
+              print(_currSugars);
+              print(_currStren);
+            },
           ),
         ],
       ),
